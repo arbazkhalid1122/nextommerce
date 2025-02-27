c41d30de1997e44dff54906aa7f382f14c782d1c
AhmedReyyan <reyyanahmed085@gmail.com>
2025-02-27 12:25:53 +0500

    button

--- pages/user/product/[id]/index.js
+++ pages/user/product/[id]/index.js
@@ -20,7 +20,7 @@ const ProductPage = () => {
   const product = products[router.query.id - 1];
 
   return (
-    <div className="max-w-7xl mx-auto p-4">
+    <div className="max-w-7xl mx-auto p-4 ">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
         {/* Left Column - Images */}
         <div className="space-y-4">
@@ -91,9 +91,9 @@ const ProductPage = () => {
                 Edit Product
               </button>
             ) : (
-              <>
+              < div className="  fixed lg:static  flex  left-[-10px] lg:flex-col lg:gap-3 lg:w-full  bottom-0 w-[110%]">
                 <button
-                  className="w-full bg-black text-white py-3 rounded-lg font-medium"
+                  className="w-full bg-black buynow lg:transform text-white py-3 lg:rounded-lg font-medium"
                   onClick={() => {
                     addToCart(product);
                     router.push("/user/checkout");
@@ -102,12 +102,12 @@ const ProductPage = () => {
                   Buy Now
                 </button>{" "}
                 <button
-                  className="w-full bg-white text-black border py-3 rounded-lg font-medium"
+                  className="w-full bg-white  buynow lg:transform text-black border py-3 lg:rounded-lg font-medium"
                   onClick={() => addToCart(product)}
                 >
                   Add to Cart
                 </button>
-              </>
+              </div>
             )}
 
             <div>
